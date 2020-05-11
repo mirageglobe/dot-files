b90b86f81fed7b1a6f4a7d80c43c71f5b538f3aa
jimmylim <mirageglobe@gmail.com>
2020-05-11 13:48:51 +0100

    update python accessor

--- vim-nanotemplate/tpl.python.arr.py
+++ vim-nanotemplate/tpl.python.arr.py
@@ -1,3 +1,3 @@
-mylist = ['physics', 'chemistry', 1997, 2000]           # mutable - can change
-mytuple = ['physics', 'chemistry', 1997, 2000]          # immutable - no change
-mydict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}   # key value type list
+mylist = ['physics', 'chemistry', 1997, 2000]           # mutable - can change, print mylist[0]
+mytuple = ['physics', 'chemistry', 1997, 2000]          # immutable - no change, print mytuple[0]
+mydict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}   # key value type list, print mydict['Name']
