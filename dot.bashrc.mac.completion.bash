7765bcae6170ece1d3b45c4febcf0a6fa69a378a
Jimmy Lim <mirageglobe@gmail.com>
2021-04-13 12:48:13 +0100

    update bash completion

--- dot.bashrc.mac.completion.bash
+++ dot.bashrc.mac.completion.bash
@@ -5,20 +5,22 @@ printf "%s" "[+] completion "
 # === === checks if bash version is 5
 BASH_VERSION_5=false
 
-if echo ${BASH_VERSION} | grep -q "^5\."; then
-  BASH_VERSION_5=true
-fi
+#if echo ${BASH_VERSION} | grep -q "^5\."; then
+  # BASH_VERSION_5=true
+# fi
 
-if ${BASH_VERSION_5} = true; then
-  printf "%s" "bash(v5+) "
-else
-  printf "%s" "bash(v?) "
-fi
+# if ${BASH_VERSION_5} = true; then
+#   printf "%s" "bash(v5+) "
+#   export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+#   [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+# else
+#   printf "%s" "bash(v?) "
+# fi
 
-if ${BASH_VERSION_5} = true; then
-  export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
-  [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
-fi
+# === === bash 5 completion
+# # add following to .bashrc to enable
+# [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+
 
 # === === git completion
 # # add following to .bashrc to enable
