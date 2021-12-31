cf8dc176d452af0f887e943445c921cbc60fd733
Jimmy Lim <mirageglobe@gmail.com>
2021-12-31 22:36:51 +0000

    update to completion

--- dot.bashrc.mac.completion.bash
+++ dot.bashrc.mac.completion.bash
@@ -1,37 +1,35 @@
-# === bash completions
+# dotfile completion
+# [[ -f ~/dot-files/dot.bashrc.mac.completion.bash ]] && . "$HOME/dot-files/dot.bashrc.mac.completion.bash"
 
 printf "%s" "[+] completion "
 
-# === === checks if bash version is 5
+# check if bash version is 5
+BASH_VERSION_3=true
 BASH_VERSION_5=false
 
-#if echo ${BASH_VERSION} | grep -q "^5\."; then
-  # BASH_VERSION_5=true
-# fi
+if echo "${BASH_VERSION}" | grep -q "^5\."; then
+  BASH_VERSION_3=false
+  BASH_VERSION_5=true
+fi
 
-# if ${BASH_VERSION_5} = true; then
-#   printf "%s" "bash(v5+) "
-#   export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
-#   [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
-# else
-#   printf "%s" "bash(v?) "
-# fi
+if [[ $BASH_VERSION_5 ]]; then
+  printf "%s" "- bash 5.x "
+  # export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+  # [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+  # https://formulae.brew.sh/formula/bash-completion@2
+  [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && \
+    . "$(brew --prefix)/etc/profile.d/bash_completion.sh" && \
+    printf "%s" "loaded "
+else
+  printf "%s" "- bash 3.x "
+fi
 
-# === === bash 5 completion
-# # add following to .bashrc to enable
+# bash 5 completion - add following to .bashrc to enable
 # [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+# [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
 
+# git completion - add following to .bashrc to enable
+# [[ -f ~/dot-files/dot.completion.git.bash ]] && "$HOME/dot-files/dot.completion.git.bash"
 
-# === === git completion
-# # add following to .bashrc to enable
-# if [ -f ~/dot-files/dot.completion.git.bash ]; then
-#   source ~/dot-files/dot.completion.git.bash
-# fi
-
-# === === make completion
-# # add following to .bashrc to enable
-# if [ -f ~/dot-files/dot.bash-completion.make-menu.bash ]; then
-#   source ~/dot-files/dot.bash-completion.make-menu.bash
-# fi
-
-# === bash loader end
+# make completion - add following to .bashrc to enable
+# [[ -f ~/dot-files/dot.bash-completion.make-menu.bash ]] && . "$HOME/dot-files/dot.bash-completion.make-menu.bash"
