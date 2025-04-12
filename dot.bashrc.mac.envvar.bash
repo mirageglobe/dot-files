118c3d31f64c6ab8e5ad8e5b297968acbbb74552
Jimmy Lim <mirageglobe@gmail.com>
2025-04-12 14:16:13 +0100

    trim envvars

--- dot.bashrc.mac.envvar.bash
+++ dot.bashrc.mac.envvar.bash
@@ -25,21 +25,3 @@ export GREP_OPTIONS='--color=auto'                  # common
 export NOW=$(date +"%Y%m%d-000000")                 # common
 export EDITOR=vim                                   # common
 export TERM=xterm-256color                          # common
-
-# ----------------------------------------------------------------- fzf ----- #
-
-# ensure install fzf via brew
-# to support terminal and keybindings, run : /usr/local/opt/fzf/install
-export FZF_COMPLETION_TRIGGER='??'                                      # keybind fzf trigger
-export FZF_COMPLETION_OPTS='--height 30% --border --info=inline'        # apply default flags for completion
-export FZF_DEFAULT_OPTS='--height 30% --border --info=inline'           # apply default flags for command, do not add preview
-
-# ---------------------------------------------------------------- java ----- #
-
-# use sdkman.io to install temurin (adoptopenjdk replacement)
-# example: sdk install java 17.0.0-tem
-#
-# if [ -z $JAVA_HOME ]; then
-#   printf "%s" "[+] java "
-#   export JAVA_HOME=$(/usr/libexec/java_home)
-# fi
